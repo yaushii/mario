43e443024d238fb93e0b81603b02b2a536e8f676
Justine <justine.telmann@gmail.com>
2020-05-06 21:40:50 +0200

    modification class Mario

--- src/com/Personage/Mario.java
+++ src/com/Personage/Mario.java
@@ -1,5 +1,26 @@
 package com.Personage;
 
-public class Mario {
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+
+public class Mario extends Personnage{
+
+	/*****VARIBLES *****/
+	private Image imgMario;
+	private ImageIcon icoMario;
+	
+	
+	public Mario(int x, int y) {
+		super(x, y,28,50);
+		
+		this.icoMario = new ImageIcon("src/image/marioDroite.png");
+		this.imgMario = this.icoMario.getImage();
+	}
+	/***** GETTERS *****/
+	
+	public Image getImgMario(){
+		return imgMario;
+	}
 
 }

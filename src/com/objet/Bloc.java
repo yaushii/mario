30da12e5799e5fc98bfffe73c3ddb12c4ff747d3
Justine <justine.telmann@gmail.com>
2020-05-06 22:57:09 +0200

    jout class bloc et tuyauRouge

--- src/com/objet/Bloc.java
+++ src/com/objet/Bloc.java
@@ -1,5 +1,23 @@
 package com.objet;
 
-public class Bloc {
+import java.awt.Image;
 
+import javax.swing.ImageIcon;
+
+public class Bloc extends Objet {
+
+	private Image imgBloc;
+	private ImageIcon icoBloc;
+
+	public Bloc(int x, int y) {
+		super(x, y, 41, 65);
+
+		this.icoBloc = new ImageIcon(getClass().getResource("/image/bloc.png"));
+		this.imgBloc = this.icoBloc.getImage();
+	}
+
+	/***** GETTERS *****/
+	public Image getImgBloc() {
+		return imgBloc;
+	}
 }
--- src/com/objet/TuyauRouge.java
+++ src/com/objet/TuyauRouge.java
@@ -1,5 +1,24 @@
 package com.objet;
 
-public class TuyauRouge {
+import java.awt.Image;
 
+import javax.swing.ImageIcon;
+
+public class TuyauRouge extends Objet {
+
+	private Image imgTuyau;
+	private ImageIcon icoTuyau;
+
+	public TuyauRouge(int x, int y) {
+		super(x, y, 41, 65);
+
+		this.icoTuyau = new ImageIcon(getClass().getResource(
+				"/image/TuyauRouge.png"));
+		this.imgTuyau = this.icoTuyau.getImage();
+	}
+
+	/***** GETTERS *****/
+	public Image getImgTuyau() {
+		return imgTuyau;
+	}
 }
